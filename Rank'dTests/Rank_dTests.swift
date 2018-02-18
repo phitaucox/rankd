2e88b167acf58e3fc6a5f1ebf74c1db635aad948
Cox, Christian <Christian.Cox@espn.com>
2018-02-18 16:54:56 -0500

    Initial Commit

---
+++ Rank'dTests/Rank_dTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rank_dTests.swift
+//  Rank'dTests
+//
+//  Created by Cox, Christian on 2/18/18.
+//  Copyright © 2018 Cox, Christian. All rights reserved.
+//
+
+import XCTest
+@testable import Rank_d
+
+class Rank_dTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
